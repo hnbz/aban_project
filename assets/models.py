e4b1cdbebe64d720271b9f3802f1f9306499d7cf
hanynv76 <hanynv76@gmail.com>
2024-11-21 01:14:27 +0330

    feat: define asset and wallet models to store related data

--- assets/models.py
+++ assets/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from aban.base_models import BaseModel
+from crypto_currencies.models import CryptoCurrency
+
 
 # Create your models here.
+class Wallet(BaseModel):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    balance = models.BigIntegerField()
+
+class Asset(BaseModel):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=20, decimal_places=8)
+    crypto_currency = models.ForeignKey(CryptoCurrency, on_delete=models.RESTRICT)
