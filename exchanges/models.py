137b428f7f6a38ab98bc0bf0d3454b7d47c4725b
hanynv76 <hanynv76@gmail.com>
2024-11-21 01:14:06 +0330

    feat: define exchange model to store related data

--- exchanges/models.py
+++ exchanges/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+from aban.base_models import BaseModel
+from crypto_currencies.models import CryptoCurrency
 # Create your models here.
+
+class Exchange(BaseModel):
+    class ExchangeStatusChoice(models.TextChoices):
+        PENDING = "pending"
+        DONE = "done"
+    user = models.ForeignKey(get_user_model(), on_delete=models.RESTRICT)
+    crypto_currency = models.ForeignKey(CryptoCurrency, on_delete=models.RESTRICT)
+    crypto_price_at_the_time = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.DecimalField(max_digits=20, decimal_places=8)
+    status = models.CharField(choices=ExchangeStatusChoice)
