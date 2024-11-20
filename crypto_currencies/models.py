4850302eda2399a20f5053cb16431b3930a20744
hanynv76 <hanynv76@gmail.com>
2024-11-21 01:13:43 +0330

    feat: define crypto currency model to store related data

--- crypto_currencies/models.py
+++ crypto_currencies/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
+from aban.base_models import BaseModel
+
 
 # Create your models here.
+class CryptoCurrency(BaseModel):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
