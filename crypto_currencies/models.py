fb77e888b8a0065d8c9e0667b6d338b05ecbb6dc
hanynv76 <hanynv76@gmail.com>
2024-11-22 14:41:01 +0330

    refactor: crpto currenc model - add symbol and price_buy and price_sell column

--- crypto_currencies/models.py
+++ crypto_currencies/models.py
@@ -6,4 +6,9 @@ from aban.base_models import BaseModel
 # Create your models here.
 class CryptoCurrency(BaseModel):
     name = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    symbol = models.CharField(max_length=255, unique=True)
+    price_buy = models.DecimalField(max_digits=20, decimal_places=10)
+    price_sell = models.DecimalField(max_digits=20, decimal_places=10)
+
+    def __str__(self):
+        return f'{self.name} ({self.symbol})'
