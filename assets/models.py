2bb71c12eb08055bd4202094689ddc2c41f4a57b
hanynv76 <hanynv76@gmail.com>
2024-11-22 14:42:59 +0330

    refactor: asset model quantity column and wallet model balance column

--- assets/models.py
+++ assets/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from aban.base_models import BaseModel
 from crypto_currencies.models import CryptoCurrency
+from django.core.validators import MinValueValidator
 
 
-# Create your models here.
 class Wallet(BaseModel):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
-    balance = models.BigIntegerField()
+    balance = models.BigIntegerField(default=0, validators=[MinValueValidator(limit_value=0)])
 
 class Asset(BaseModel):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    quantity = models.DecimalField(max_digits=20, decimal_places=8)
+    quantity = models.DecimalField(max_digits=20, decimal_places=8, default=0)
     crypto_currency = models.ForeignKey(CryptoCurrency, on_delete=models.RESTRICT)
