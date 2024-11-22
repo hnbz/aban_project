68d8460c770722fd86f598047251c46f7f7f92b0
hanynv76 <hanynv76@gmail.com>
2024-11-22 18:12:59 +0330

    refactor: exception raising class

--- assets/views.py
+++ assets/views.py
@@ -2,7 +2,7 @@ from rest_framework.generics import ListCreateAPIView, ListAPIView, RetrieveAPIV
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from django.db import transaction
-from rest_framework.exceptions import APIException
+from aban.exceptions import BadRequest, ServiceUnavailable
 from assets.models import Wallet, Asset, Deposit
 from assets.serializers import AssetSerializer, DepositSerializer, WalletSerializer
 from rest_framework.response import Response
@@ -15,15 +15,18 @@ class DepositView(ListCreateAPIView):
     def perform_create(self, serializer: DepositSerializer):
         user = self.request.user
         if not user.is_verified:
-            raise APIException(detail='verification required', code=status.HTTP_400_BAD_REQUEST)
+            raise BadRequest(detail='verification required', code=status.HTTP_400_BAD_REQUEST)
         data: dict = serializer.validated_data
-        with transaction.atomic():
-            wallet = Wallet.objects.filter(user=user).first()
-            after_balance = wallet.balance + data.get('amount')
-            serializer.save(wallet=wallet, before_balance=wallet.balance, after_balance=after_balance)
-            wallet.balance = after_balance
-            wallet.save()
-            return Response(status.HTTP_201_CREATED)
+        try:
+            with transaction.atomic():
+                wallet = Wallet.objects.filter(user=user).first()
+                after_balance = wallet.balance + data.get('amount')
+                serializer.save(wallet=wallet, before_balance=wallet.balance, after_balance=after_balance)
+                wallet.balance = after_balance
+                wallet.save()
+                return Response(status.HTTP_201_CREATED)
+        except:
+            raise ServiceUnavailable(detail='verification required', code=status.HTTP_503_SERVICE_UNAVAILABLE)
 
     def get_queryset(self):
         wallet = Wallet.objects.filter(user=self.request.user).first()
--- users/views.py
+++ users/views.py
@@ -6,8 +6,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from users.serializers import UserSerializer, LoginSerializer
 from django.contrib.auth import authenticate
-from rest_framework.exceptions import APIException
-
+from aban.exceptions import Unauthorized, BadRequest
 
 # Create your views here.
 class Register(CreateAPIView):
@@ -16,7 +15,7 @@ class Register(CreateAPIView):
 
     def perform_create(self, serializer: UserSerializer):
         if User.objects.filter(mobile = serializer.validated_data.get('mobile')):
-            raise APIException(detail='user with this mobile number already exists', code=status.HTTP_400_BAD_REQUEST)
+            raise BadRequest(detail='user with this mobile number already exists', code=status.HTTP_400_BAD_REQUEST)
         User.objects.create_user(**serializer.validated_data)
         return Response(status=status.HTTP_201_CREATED)
 
@@ -32,4 +31,4 @@ class Login(GenericAPIView):
         if user:
             token, token_status = Token.objects.get_or_create(user=user)
             return Response(data = {"token": token.key}, status=201 if token_status else 200)
-        raise APIException(detail='Invalid credential', code=status.HTTP_401_UNAUTHORIZED)
+        raise Unauthorized(detail='Invalid credential', code=status.HTTP_401_UNAUTHORIZED)
