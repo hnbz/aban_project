61c4ec13e277e864ba9f3994da0d7972206fb16e
hanynv76 <hanynv76@gmail.com>
2024-11-22 15:15:48 +0330

    fix: path url name of assets

--- assets/urls.py
+++ assets/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("wallet/", views.WalletView.as_view(), name="Exchange"),
-    path("", views.AssetView.as_view(), name="Exchange"),
-    path("deposit/", views.DepositView.as_view(), name="Exchange"),
+    path("wallet/", views.WalletView.as_view(), name="Wallet"),
+    path("", views.AssetView.as_view(), name="Asset"),
+    path("deposit/", views.DepositView.as_view(), name="Deposit"),
 ]
