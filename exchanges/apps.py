8fa5b6ca22438388ea3dfe0b61de17efd88a6200
hanynv76 <hanynv76@gmail.com>
2024-11-22 15:05:47 +0330

    feat: register exchange signal

--- exchanges/apps.py
+++ exchanges/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ExchangesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'exchanges'
+
+    def ready(self):
+        import exchanges.signals
