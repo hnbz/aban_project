31a38a17ca2743c5da10b7dc57aef48e6327ab22
hanynv76 <hanynv76@gmail.com>
2024-11-22 16:23:02 +0330

    refactor: make email and national code nullable

--- users/models.py
+++ users/models.py
@@ -9,9 +9,8 @@ class UserManager(BaseUserManager):
             raise ValueError("The mobile field is required.")
 
         email = extra_fields.get('email')
-        if not email:
-            raise ValueError("The email field is required.")
-        extra_fields['email'] = self.normalize_email(email)
+        if email:
+            extra_fields['email'] = self.normalize_email(email)
 
         user = self.model(mobile=mobile, **extra_fields)
         user.set_password(password)
@@ -21,7 +20,9 @@ class UserManager(BaseUserManager):
     def create_superuser(self, mobile: str, password: str = None, **extra_fields):
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_staff', True)
-
+        email = extra_fields.get('email')
+        if not email:
+            raise ValueError("The email field is required.")
         if not extra_fields.get('is_superuser'):
             raise ValueError("Superuser must have is_superuser=True.")
         if not extra_fields.get('is_staff'):
@@ -32,12 +33,13 @@ class UserManager(BaseUserManager):
 
 class User(AbstractUser):
     mobile = models.CharField(max_length=10, unique=True)
-    national_code = models.CharField(max_length=10, unique=True)
-    email = models.EmailField(unique=True)
+    national_code = models.CharField(max_length=10, unique=True, null=True)
+    email = models.EmailField(unique=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = UserManager()
     USERNAME_FIELD = 'mobile'
+    username = None
 
     @property
     def is_verified(self):
