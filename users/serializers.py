73605d849333100464a7d7bb621d933acafe8b06
hanynv76 <hanynv76@gmail.com>
2024-11-22 18:14:20 +0330

    refactor: user serializers fields

--- users/serializers.py
+++ users/serializers.py
@@ -4,8 +4,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta:
-        fields = ['mobile']
-        exclude = ('is_staff', 'is_superuser', 'created_at', 'updated_at')
+        fields = ['mobile', 'password']
         model = User
 
 class LoginSerializer(serializers.Serializer):
