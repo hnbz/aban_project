4c28d7f2b80cdfc7a08b684a1b43aa14c01f6a61
hanynv76 <hanynv76@gmail.com>
2024-11-22 15:41:39 +0330

    fix: user serializer created at field

--- users/serializers.py
+++ users/serializers.py
@@ -4,7 +4,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta:
-        exclude = ('is_staff', 'is_superuser', ' created_at', 'updated_at')
+        exclude = ('is_staff', 'is_superuser', 'created_at', 'updated_at')
         model = User
 
 class LoginSerializer(serializers.Serializer):
