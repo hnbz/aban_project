814dd30b5af7b15ad88bddc38ffc1c677562e22c
hanynv76 <hanynv76@gmail.com>
2024-11-22 14:50:48 +0330

    feat: Implement Register API for user registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.authtoken.models import Token
+from .models import User
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from users.serializers import UserSerializer, LoginSerializer
+from django.contrib.auth import authenticate
+from rest_framework.exceptions import APIException
+
 
 # Create your views here.
+class Register(CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer: UserSerializer):
+        if User.objects.filter(mobile = serializer.validated_data.get('mobile')):
+            raise APIException(detail='user with this mobile number already exists', code=status.HTTP_400_BAD_REQUEST)
+        User.objects.create_user(**serializer.validated_data)
+        return Response(status=status.HTTP_201_CREATED)
